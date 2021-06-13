0b6fb6ad7de12e0e309d4c55372f5728ee3927ee
MURALIDHAR-831 <85813811+MURALIDHAR-831@users.noreply.github.com>
2021-06-13 15:48:51 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main 
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter your name");
+        String name =sc.next();
+         System.out.println("enter your acc no");
+        int  accno = sc.nextInt();
+         System.out.println("enter your act type");
+        String acttype = sc.next();
+        
+         System.out.println("your name: " +name);
+         System.out.println("your acc no: " +accno);
+         System.out.println("your acc tye: " +acttype);
+        
+    }
+}
